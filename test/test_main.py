6392983793e8e816920114bd98814f67beb25ab9
Andrej Ocenas <mr.ocenas@gmail.com>
2017-06-13 15:30:03 +0200

    -

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,15 @@
 import unittest
+import requests
+SERVER_URL = 'http://localhost:5000'
 
 
 class MainTestCase(unittest.TestCase):
     def test_main(self):
         self.assertEqual(True, True)
+
+
+class APITestCase(unittest.TestCase):
+    def test_test(self):
+        campaign_id = '23842563749550662'
+        res = requests.get(f'{SERVER_URL}/test')
+        self.assertIn(res.status_code, [200])
